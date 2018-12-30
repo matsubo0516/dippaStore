a7434f8181b33b93a576c4b49209048df806cb8e
松本直樹 <n-matsumoto@hone-corp.jp>
2018-12-30 23:02:55 +0900

    あげなおし

--- dippa_store/AppDelegate.swift
+++ dippa_store/AppDelegate.swift
@@ -5,7 +5,6 @@
 //  Created by 松本直樹 on 2018/12/08.
 //  Copyright © 2018年 松本直樹. All rights reserved.
 //
-// コメントしました
 
 import UIKit
 
---
+++ dippa_store/MyMenuCamera.swift
@@ -0,0 +1,9 @@
+//
+//  MyMenuCamera.swift
+//  dippa_store
+//
+//  Created by 松本直樹 on 2018/12/30.
+//  Copyright © 2018年 松本直樹. All rights reserved.
+//
+
+import Foundation
