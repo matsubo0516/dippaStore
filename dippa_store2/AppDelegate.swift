bebfe2936995a584ca14d1075f2f191ca05b393d
松本直樹 <n-matsumoto@hone-corp.jp>
2019-01-01 17:53:45 +0900

    あげなおし

--- dippa_store2/AppDelegate.swift
+++ dippa_store2/AppDelegate.swift
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.swift
-//  dippa_store
+//  dippa_store2
 //
-//  Created by 松本直樹 on 2018/12/08.
-//  Copyright © 2018年 松本直樹. All rights reserved.
+//  Created by 松本直樹 on 2019/01/01.
+//  Copyright © 2019年 松本直樹. All rights reserved.
 //
 
 import UIKit
---
+++ dippa_store2/MenuViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MenuViewController.swift
+//  dippa_store2
+//
+//  Created by 松本直樹 on 2019/01/01.
+//  Copyright © 2019年 松本直樹. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    @IBOutlet weak var MenuTextField: UITextField!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    var memo: String?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+     
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let button = sender as? UIBarButtonItem, button === self.saveButton else {
+            return
+        }
+        self.memo = self.MenuTextField.text ?? ""
+    }
+
+
+}
