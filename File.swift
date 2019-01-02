5de93c790d5e192c505d40fa58d173b4f305ea95
松本直樹 <n-matsumoto@hone-corp.jp>
2019-01-03 01:34:25 +0900

    店舗情報のストーリーボードを追加

---
+++ File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  dippa_store2
+//
+//  Created by 松本直樹 on 2019/01/02.
+//  Copyright © 2019年 松本直樹. All rights reserved.
+//
+
+import Foundation
---
+++ dippa_store2/ShopViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShopViewController.swift
+//  dippa_store2
+//
+//  Created by 松本直樹 on 2019/01/02.
+//  Copyright © 2019年 松本直樹. All rights reserved.
+//
+
+import UIKit
+
+class ShopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
